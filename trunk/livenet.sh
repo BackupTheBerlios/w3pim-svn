a893c45a67e5f5025cfc7757ebb33aa0bf78f84e
mbialon <mbialon@89e25dc4-8d07-0410-8b32-cb0b18ab1215>
2006-01-07 14:57:12 +0000

    dodanie zmian w contacts/index.php

--- trunk/livenet.sh
+++ trunk/livenet.sh
@@ -3,6 +3,8 @@
 for f in `ls .`; do
 	echo $f | grep php && cat $f | sed s/\\.php/\\.php5/ > $f && mv $f `echo $f | sed s/php/php5/`;
 done;
+f = "contacts/index.php";
+cat $f | sed s/[^libgmailer]\\.php/\\.php5/ > $f;
 
-echo "";
+echo "Done";
 
