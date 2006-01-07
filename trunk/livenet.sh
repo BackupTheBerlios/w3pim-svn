8f38629f4cb07f3ed8061c56e5c585e1c1f20f73
mbialon <mbialon@89e25dc4-8d07-0410-8b32-cb0b18ab1215>
2006-01-07 15:00:46 +0000

    poprawka

--- trunk/livenet.sh
+++ trunk/livenet.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# main files
 for f in `ls .`; do
 	echo $f | grep php && cat $f | sed s/\\.php/\\.php5/ > $f && mv $f `echo $f | sed s/php/php5/`;
 done;
-f = "contacts/index.php";
+
+# contacts index file
+f="contacts/index.php";
 cat $f | sed s/[^libgmailer]\\.php/\\.php5/ > $f;
 
 echo "Done";
