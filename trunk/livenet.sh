e81ddd88a60433422f4f5956510e9023838109bf
mbialon <mbialon@89e25dc4-8d07-0410-8b32-cb0b18ab1215>
2006-01-08 15:58:49 +0000

    upgrade php->php5 tylko dla katalogow wymienionych w skrypcie

--- trunk/livenet.sh
+++ trunk/livenet.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# main files
-for f in `ls .`; do
-	echo $f | grep php && cat $f | sed s/\\.php/\\.php5/ > $f && mv $f `echo $f | sed s/php/php5/`;
-done;
+function upgrade {
+	for f in `ls $1`; do
+		echo $f | grep php && cat $f | sed s/\\.php/\\.php5/ > $f && mv $f `echo $f | sed s/php/php5/`;
+	done;
+}
 
-# contacts index file
-f="contacts/index.php";
-cat $f | sed s/[^libgmailer]\\.php/\\.php5/ > $f;
+upgrade .
+upgrade templates
 
 echo "Done";
 
