a9df1f7310347676f51bd10a3a292635d237c7c8
kocurr <kocurr@89e25dc4-8d07-0410-8b32-cb0b18ab1215>
2006-01-10 22:26:22 +0000

    sortowania malejące (Postep i Priorytet)  edycja zadania  usuwanie zadania (opcjonalne)  niepuste pole Tytul  przekierowanie na listę zadań po dodaniu zadania  wyświetlanie nazwy użytkownika zamiast id

--- trunk/todo.php
+++ trunk/todo.php
@@ -7,25 +7,46 @@
                       echo $e->getMessage();
                       exit;
               }
-?>
-
-</BODY>
-</HTML> 
-      
-
-<?php
 
 
-$user = $u->get_id();
-echo "Uzytkownik ma id: {$user}";
+  /*
+
+ zapisywanie danych onChange na li¶cie zadañ (AJAX) (opcjonalne)
+
+ edycja zadania
+
+*/
+
 
 
 $link = mysql_connect(XHOST, XLOGIN, XPASSWD) or die(mysql_error());
 mysql_select_db(XDB) or die(mysql_error()) ;
 
-if ($HTTP_GET_VARS['akcja'] == 'dodaj')
+$user = $u->get_id();
+$query = "SELECT uzk_login FROM uzytkownicy WHERE uzk_id={$user}";
+$result = mysql_query($query) or die(mysql_error());
+$tmp = mysql_fetch_array($result, MYSQL_ASSOC);
+$user_name = $tmp['uzk_login'];
+//echo "Uzytkownik : {$tmp['uzk_login']}";
+
+if (($HTTP_GET_VARS['akcja'] == 'dodaj') || ($HTTP_GET_VARS['akcja']== 'zmien'))
 {
-	
+
+        if (strlen($HTTP_GET_VARS['tytul']) == 0)
+           {
+               header('Location: todo.php?blad=1');
+               exit;
+           }
+        if (strlen($HTTP_GET_VARS['opis']) == 0)
+           {
+               header('Location: todo.php?blad=2');
+               exit;
+           }
+       /* foreach($HTTP_GET_VARS as $k => $w)
+        {
+           echo "{$k} => {$w}<BR>";
+        }*/
+
 	$query = "SELECT pri_id FROM priorytety where pri_nazwa='{$HTTP_GET_VARS['priorytet']}'";
 	$result = mysql_query($query) or die(mysql_error());
 	$tmp = mysql_fetch_array($result, MYSQL_ASSOC);
@@ -38,43 +59,138 @@ if ($HTTP_GET_VARS['akcja'] == 'dodaj')
 	$id_kategorii = $tmp['kat_id'];
 	mysql_free_result($result);
 	
-	$query = "INSERT INTO zadania 
-		(zad_tytul,zad_opis,zad_postep,zad_data_zakonczenia, 
-		zad_pri_id, zad_kat_id, zad_uzk_id) values 
+	if ($HTTP_GET_VARS['akcja'] == 'dodaj')
+	{
+	    $query = "INSERT INTO zadania
+		(zad_tytul,zad_opis,zad_postep,zad_data_zakonczenia,
+		zad_pri_id, zad_kat_id, zad_uzk_id) values
 		('{$HTTP_GET_VARS['tytul']}','{$HTTP_GET_VARS['opis']}',0,
 		'{$HTTP_GET_VARS['rok']}-{$HTTP_GET_VARS['miesiac']}-{$HTTP_GET_VARS['dzien']}',
 		{$id_priorytetu},{$id_kategorii},{$user})";
-	$result = mysql_query($query);
-	echo 'Element zosta³ dodany!';
-	echo '<A HREF="todo.php?akcja=lista">Wyswietl liste TODO</A><BR>';
-	echo '<A HREF="todo.php">Dodaj zdarzenie TODO</A><BR>';
 	
+	
+        }
+        elseif ($HTTP_GET_VARS['akcja'] == 'zmien')
+        {
+             $query = "UPDATE zadania SET
+		zad_tytul='{$HTTP_GET_VARS['tytul']}',
+                zad_opis='{$HTTP_GET_VARS['opis']}',
+                zad_postep='{$HTTP_GET_VARS['postep']}',
+		zad_data_zakonczenia=
+                '{$HTTP_GET_VARS['rok']}-{$HTTP_GET_VARS['miesiac']}-{$HTTP_GET_VARS['dzien']}',
+		zad_pri_id={$id_priorytetu},
+                zad_kat_id={$id_kategorii}
+                WHERE zad_id={$HTTP_GET_VARS['zad_id']}";
+	
+	//        echo $query;
+        }
+        $result = mysql_query($query)or die("<BR>Zapytanie niepoprawne" . mysql_error());;
+
+ 	header('Location: todo.php?akcja=lista');
+	//echo 'Element zosta³ dodany!';
+	//echo '<A HREF="todo.php?akcja=lista">Wyswietl liste TODO</A><BR>';
+	//echo '<A HREF="todo.php">Dodaj zdarzenie TODO</A><BR>';
+ 	//echo '</BODY>';
+        //echo '</HTML>';
 	
 }
 elseif ($HTTP_GET_VARS['akcja'] == 'lista')
 {	
 //	echo '<B>Bedzimy listowac</B><BR>';
+	echo '</BODY>';
+        echo '</HTML>'; 	
 	
 	switch ($HTTP_GET_VARS['sort']) 
 	{
    		case 'data':
 			$ord_by_str = 'zad_data_zakonczenia';
-              		break;
+			$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data_desc">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
    		case 'tytul':
        			$ord_by_str = 'zad_tytul';
-			break;
+       			$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul_desc">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
 		case 'postep':
 			$ord_by_str = 'zad_postep';
-			break;
+			$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep_desc">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
 		case 'kat':
 			$ord_by_str = 'kat_nazwa';
-			break;
+			$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat_desc">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
 		case 'pri':
 			$ord_by_str = 'pri_nazwa';
-			break;
+			$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri_desc">Priorytet</A>';
+                        break;
+			
+		case 'data_desc':
+			$ord_by_str = 'zad_data_zakonczenia desc';
+              		$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
+   		case 'tytul_desc':
+       			$ord_by_str = 'zad_tytul desc';
+       			$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
+		case 'postep_desc':
+			$ord_by_str = 'zad_postep desc';
+			$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
+		case 'kat_desc':
+			$ord_by_str = 'kat_nazwa desc';
+			$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
+		case 'pri_desc':
+			$ord_by_str = 'pri_nazwa desc';
+			$link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
 		default:
 			$ord_by_str = 'zad_data_zakonczenia';
-              		break;
+		        $link_param_data = '<A HREF="todo.php?akcja=lista&sort=data">Data</A>';
+                        $link_param_tytul = '<A HREF="todo.php?akcja=lista&sort=tytul">Tytu³</A>';
+                        $link_param_postep = '<A HREF="todo.php?akcja=lista&sort=postep">Postep</A>';
+              		$link_param_kat = '<A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A>';
+                        $link_param_pri = '<A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A>';
+                        break;
 	}
 	
 	$q = 'SELECT max(zad_id) as maksimum from zadania';
@@ -98,14 +214,17 @@ elseif ($HTTP_GET_VARS['akcja'] == 'lista')
 	echo '<FORM action="todo.php" method="get">';	
 	echo '<TABLE BORDER="0">';
 //	echo '<TABLE >';
-	echo 	'<TR>
-		<TH><A HREF="todo.php?akcja=lista&sort=tytul">Tytul</A></TH>
+
+	echo 	"<TR>
+		<TH>{$link_param_tytul}</TH>
 		<TH>Opis</TH>
-		<TH><A HREF="todo.php?akcja=lista&sort=data">Data zakonczenia</A></TH>
-		<TH><A HREF="todo.php?akcja=lista&sort=postep">Postep</A></TH>
-		<TH><A HREF="todo.php?akcja=lista&sort=kat">Kategoria</A></TH>
-		<TH><A HREF="todo.php?akcja=lista&sort=pri">Priorytet</A></TH>
-		</TR>';
+		<TH>{$link_param_data}</TH>
+		<TH>{$link_param_postep}</TH>
+		<TH>{$link_param_kat}</TH>
+		<TH>{$link_param_pri}</TH>
+		<TH>Edycja</TH>
+		<TH>Usuwanie</TH>
+		</TR>";
 			
 	while ($tmp = mysql_fetch_array($result, MYSQL_ASSOC))
 	{
@@ -113,14 +232,15 @@ elseif ($HTTP_GET_VARS['akcja'] == 'lista')
 				$kolor = 'red';
 			else
 				$kolor = '';
-		
-		
+				
 		$kol_tytul = $tmp['zad_tytul'];
 		
 		$kol_opis = $tmp['zad_opis'];
 
 		$kol_data = substr($tmp['zad_data_zakonczenia'],0,10);
 
+
+                
 		$s = sprintf("%%0%dd",$max_id);
 		$numer_zad = sprintf($s, $tmp['zad_id']);
 
@@ -159,25 +279,41 @@ elseif ($HTTP_GET_VARS['akcja'] == 'lista')
 				$kol_pri .= "<OPTION SELECTED>{$tmp_pri['pri_nazwa']}";	
 		}
 		$kol_pri .= '</SELECT>';
-		
-
-		echo "<TR BGCOLOR=\"{$kolor}\"><TD>{$kol_tytul}</TD><TD>{$kol_opis}</TD><TD>{$kol_data}</TD>
-		<TD>{$kol_postep}</TD><TD>{$kol_kat}</TD><TD>{$kol_pri}</TD> </TR>";
+		$kol_usun =  "<INPUT  TYPE=\"CHECKBOX\" NAME=\"{$numer_zad}\">";
+		//$kol_usun = "<input type=\"submit\" value=\"usuñ\" name=\"akcja\">";
+		//$kol_edycja = "<input type=\"submit\" value=\"edycja\" name=\"akcja\">";
+                $kol_edycja = "<input type=\"radio\" value=\"{$numer_zad}\" name=\"doedycji\">";
+		echo "<TR BGCOLOR=\"{$kolor}\"><TD>{$kol_tytul}</TD>
+                     <TD>{$kol_opis}</TD><TD>{$kol_data}</TD>
+		     <TD>{$kol_postep}</TD><TD>{$kol_kat}</TD>
+                     <TD>{$kol_pri}</TD><TD>{$kol_edycja}</TD><TD>{$kol_usun}</TD> </TR>";
 			
 	}
 		
 	echo '</TABLE>';
 	echo '<input type="submit" value="update" name="akcja">';
-
+	echo '<input type="submit" value="usun" name="akcja">';
+	echo '<input type="submit" value="edycja" name="akcja">';
+	
 	echo '</FORM>';
 	echo "<A href=\"todo.php?akcja=lista\">Lista TODO</A><BR>";
 	echo "<A href=\"todo.php\">Dodaj zdarzenie TODO</A><BR>";
+	echo '</BODY>';
+        echo '</HTML>';
 }
 elseif ($HTTP_GET_VARS['akcja'] == 'update')
 {
+       /* echo '</BODY>';
+        echo '</HTML>';
 
-	echo '<B>Update\'ujemy liste</B><BR>';
-	
+        foreach($HTTP_GET_VARS as $k => $w)
+        {
+           echo "{$k} => {$w}<BR>";
+        }
+
+       	echo '</BODY>';
+        echo '</HTML>';*/
+        	
 	$l_elementow = (int)(count($HTTP_GET_VARS)/3);
 	//echo "Liczba elementow tablicy = {$l_elementow}<BR>";
 	reset($HTTP_GET_VARS);
@@ -206,13 +342,184 @@ elseif ($HTTP_GET_VARS['akcja'] == 'update')
 		
 		
 	}
-	echo "<A href=\"todo.php?akcja=lista\">Lista TODO</A><BR>";
-	echo "<A href=\"todo.php\">Dodaj zdarzenie TODO</A><BR>";
+        header('Location: todo.php?akcja=lista');
+	//echo "<A href=\"todo.php?akcja=lista\">Lista TODO</A><BR>";
+	//echo "<A href=\"todo.php\">Dodaj zdarzenie TODO</A><BR>";
 
 }
+elseif   ($HTTP_GET_VARS['akcja'] == 'usun')
+{
+       // echo '</BODY>';
+       // echo '</HTML>';
+       // echo "Nacisnieto usun<BR>";
+        foreach($HTTP_GET_VARS as $k => $w)
+        {
+              //echo "{$k} => {$w}<BR>";
+              
+              if ($w == 'on')
+              {
+                 $query = "UPDATE zadania SET zad_postep=100 WHERE zad_id = {$k}";
+		 mysql_query($query) or die('aaaa' . mysql_error());
+
+              }
+
+
+        }
+       header('Location: todo.php?akcja=lista');
+
+
+}
+elseif ($HTTP_GET_VARS['akcja'] == 'edycja')
+{
+        if (array_key_exists('doedycji', $HTTP_GET_VARS))
+        {
+           echo '</BODY>';
+           echo '</HTML>';
+
+
+           $query = "SELECT zad_id, zad_tytul,zad_opis,zad_postep,zad_data_zakonczenia,
+                     kat_nazwa, pri_nazwa
+                     FROM zadania z, uzytkownicy u, priorytety p, kategorie k
+                     WHERE z.zad_id={$HTTP_GET_VARS['doedycji']} AND
+                     u.uzk_id={$user} AND z.zad_kat_id=k.kat_id AND
+                     z.zad_pri_id=p.pri_id";
+
+	   $result = mysql_query($query) or die(mysql_error());
+	   $wynik = mysql_fetch_array($result, MYSQL_ASSOC);
+	   list($rok, $miesiac, $dzien) = sscanf($wynik['zad_data_zakonczenia'], "%d-%d-%d");
+
+
+           $sel_dzien = "<SELECT NAME=\"dzien\" >";
+	   
+	   for ($a = 1; $a <= 31; $a++)
+	   {
+		if ($a == $dzien)
+			$tmp = "<OPTION SELECTED> {$a}";
+		else
+			$tmp = "<OPTION> {$a}";
+		$sel_dzien .= $tmp;
+	   }
+
+	$sel_dzien =$sel_dzien .'</SELECT>';
+	$sel_miesiac = "<SELECT NAME=\"miesiac\" >";
+
+	for ($a = 1; $a <= 12; $a++)
+	{
+		if($a == $miesiac)
+			$tmp = "<OPTION SELECTED> {$a}";
+		else
+			$tmp = "<OPTION> {$a}";
+		$sel_miesiac .= $tmp;
+	}
+
+	$sel_miesiac = $sel_miesiac .'</SELECT>';
+	$pole_rok = "<INPUT TYPE=\"TEXT\" NAME=\"rok\" MAXLENGTH=\"4\" VALUE={$rok}>";
+	
+	echo '<FORM action="todo.php" method="get">';
+
+	echo '<TABLE BORDER="1">';
+
+	echo "<TR><TD WIDTH=\"200\"> Data </TD><TD>{$sel_dzien} {$sel_miesiac} {$pole_rok}</TD></TR>";
+
+	$pole_tytul = "<INPUT TYPE=\"TEXT\" NAME=\"tytul\" VALUE=\"{$wynik['zad_tytul']}\" MAXLENGTH=\"255\">";
+	echo "<TR><TD WIDTH=\"200\"> Tytu³ </TD><TD>{$pole_tytul}</TD></TR>";
+
+	$pole_opisu = "<TEXTAREA NAME=\"opis\" ROWS=\"4\" COLS=\"50\" >{$wynik['zad_opis']}</TEXTAREA>";
+	echo "<TR><TD WIDTH=\"200\"> Opis </TD><TD>{$pole_opisu}</TD></TR>";
+
+
+
+	$query = 'SELECT pri_nazwa FROM priorytety ORDER BY pri_id';
+	$result = mysql_query($query) or die(mysql_error());
+	echo '<TR><TD>Priorytet, Kategoria, Postep</TD><TD>';
+
+	$sel_priorytet = "Priorytet <SELECT NAME=\"priorytet\">";
+	while ($tmp1 = mysql_fetch_array($result, MYSQL_ASSOC))
+	{
+		foreach($tmp1 as $wartosc)
+		{
+		        if ($wartosc == $wynik['pri_nazwa'])
+                             $tmp = "<OPTION SELECTED> {$wartosc}";
+		        else    			
+                             $tmp = "<OPTION> {$wartosc}";
+			$sel_priorytet = $sel_priorytet . $tmp;
+		}
+	
+	}
+	echo "{$sel_priorytet}" . '</SELECT>';
+	
+	
+        $query = 'SELECT kat_nazwa FROM kategorie ORDER BY kat_id';
+	$result = mysql_query($query) or die(mysql_error());
+
+	$sel_kat = " Kategoria <SELECT NAME=\"kategoria\">";
+	while ($tmp1 = mysql_fetch_array($result, MYSQL_ASSOC))
+	{
+		foreach($tmp1 as $wartosc)
+		{
+		        if ($wartosc == $wynik['kat_nazwa'])
+			    $tmp = "<OPTION SELECTED> {$wartosc}";
+                        else
+                            $tmp = "<OPTION> {$wartosc}";
+			$sel_kat = $sel_kat . $tmp;
+		}
+	
+	}
+	    	
+	echo "{$sel_kat}" . '</SELECT>';
+	
+		
+	$sel_postep = "Postep <SELECT NAME=\"postep\">";
+	
+	for($a = 0; $a <= 100; $a += 10)
+	{
+	        if ($a == $wynik['zad_postep'])
+                      $tmp = "<OPTION SELECTED> {$a}";
+	        else    			
+                      $tmp = "<OPTION> {$a}";
+	              $sel_postep = $sel_postep . $tmp;
+	}
+	
+	
+	echo "{$sel_postep}" . '</SELECT>';
+	
+	echo '</TD></TR>';
+	echo '<TR><TD> </TD><TD><input type="submit" value="zmien" name="akcja"></TD></TR>';
+
+
+
+	echo '</TABLE>';
+	echo "<input type=hidden value=\"{$HTTP_GET_VARS['doedycji']}\" name=\"zad_id\">";
+	echo '</FORM>';
+
+           echo '</BODY>';
+           echo '</HTML>';
+           
+        }
+        else
+        {
+          header('Location: todo.php?akcja=lista');
+        }
+
+        
+        
+        
+}
+
 else
 {
-	$data = getdate();
+	echo '</BODY>';
+        echo '</HTML>';
+        foreach($HTTP_GET_VARS as $k => $w)
+        {
+           echo "{$k} => {$w}<BR>";
+        }	
+	if ($HTTP_GET_VARS['blad'] == 1)
+	   echo "Pole zawieraj±ce tytu³ nie mo¿e byæ puste!<BR>";
+	if ($HTTP_GET_VARS['blad'] == 2)
+	   echo "Pole zawieraj±ce opis nie mo¿e byæ puste!<BR>";
+		
+        $data = getdate();
 	$sel_dzien = "<SELECT NAME=\"dzien\" >";
 	for ($a = 1; $a <= 31; $a++)
 	{
@@ -292,12 +599,12 @@ else
 	echo '</FORM>';
 	echo "<A href=\"todo.php?akcja=lista\">Lista TODO</A><BR>";
 	echo "<A href=\"todo.php\">Dodaj zdarzenie TODO</A><BR>";
-	
+    	echo '</BODY>';
+        echo '</HTML>';
 
 }
 
 
 
 ?>
-</BODY>
-</HTML>
+
